1cac3b290c3749c67903ee4ccbda69d6132a0034
Marc Le <marc.le@study.hs-duesseldorf.de>
2026-01-18 18:32:38 +0100

    added the right functions in fair_sharer.py and test_fair_sharer.py

--- fairsharer_project/fairsharer_package/fair_sharer.py
+++ fairsharer_project/fairsharer_package/fair_sharer.py
@@ -1,29 +1,47 @@
-"""Documentation about the fairsharer_package module."""
+def fair_sharer(values=list, num_iterations=int, share=0.1)->list:
+    """Runs num_iterations.
+In each iteration the highest value in "values" gives a fraction (share)
+to both the left and right neighbor. The leftmost field is considered
+the neightbor of the rightmost field.
+Examples:
+fair_sharer([0, 1000, 800, 0], 1) --> [100, 800, 900, 0]
+fair_sharer([0, 1000, 800, 0], 2) --> [100, 890, 720, 90]
+Args
+values:
+1D array of values (list or numpy array)
+num_iteration:
+Integer to set the number of iterations
+"""
+    for _ in range(num_iterations):
 
+        #searching for the highest value in the list
+        max_val = max(values)
 
-# FIXME: put actual code here
-def hello(name):
-    """Say hello.
+        #getting the index of the highest value
+        max_index = values.index(max_val)
+        
+        #calculating the share of the highest value, which will be passed on
+        sharing = max_val*share
 
-    Function docstring using Google docstring style.
+        last_indx = len(values)-1
 
-    Args:
-        name (str): Name to say hello to
+        if max_index == 0: 
+            values[last_indx] += sharing
+        else:
+            values[max_index-1] += sharing        
 
-    Returns:
-        str: Hello message
+        if max_index == last_indx:
+            values[0] += share
+        else:
+            values[max_index+1] += sharing
+        
 
-    Raises:
-        ValueError: If `name` is equal to `nobody`
+        #subtracting the shares from the highest value
+        values[max_index] -= 2*sharing
 
-    Example:
-        This function can be called with `Jane Smith` as argument using
+    #returning new list with adjusted values    
+    return values
 
-        >>> from fairsharer_package.my_module import hello
-        >>> hello('Jane Smith')
-        'Hello Jane Smith!'
-
-    """
-    if name == "nobody":
-        raise ValueError("Cannot say hello to nobody")
-    return f"Hello {name}!"
+print(fair_sharer([0, 1000, 800, 0], 1))
+print(fair_sharer([0, 1000, 800, 0], 2))
+print(fair_sharer([0, 1000, 800, 0], 3))
---
+++ fairsharer_project/tests/test_fair_sharer.py
@@ -0,0 +1,21 @@
+"""Tests for the fairsharer_package.fair_sharer module.
+"""
+import pytest
+
+from fairsharer_package.fair_sharer import fair_sharer
+
+
+def test_fair_sharer_single_iteration():
+    result = fair_sharer([0, 1000, 800, 0], 1)
+    assert result == [100, 800, 900, 0]
+
+
+def test_fair_sharer_two_iterations():
+    result = fair_sharer([0, 1000, 800, 0], 2)
+    assert result == [100, 890, 720, 90]
+
+
+def test_fair_sharer_conservation():
+    values = [10, 20, 30]
+    result = fair_sharer(values, 10)
+    assert sum(result) == sum(values)
